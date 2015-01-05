de285f7fc17231c724246af9f73c623d6c448f18
Zexorz <metonator@gmail.com>
2015-01-05 21:59:13 +0100

    Revert "And the rest of files..."
    This reverts commit 0e1a9b94b74b26f79eef9725b581722db5f68d62.

--- Loader/inc/command_DEBUG.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if($exp2['3'] == ":!fbdebug") {
-		$expwed = print_r($exp2, true);
-		$expwed = str_replace(PHP_EOL, "", $expwed);
-		fputs($socket,"PRIVMSG ".$channelname." :".$expwed." \r\n");
-	}
-?>
--- Loader/inc/parse_REDTUBE.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if($to['host'] == 'www.redtube.com') {
-	fputs($socket,"PRIVMSG ".$channelname." :FAP FAP FAP FAP FAP FAP...\r\n");
-	fputs($socket,"PRIVMSG ChanServ :ADDTIMEDBAN ".$channelname." ".$get_nickname." 1m ... FAP FAP FAP No porn allowed ".$get_nickname."\r\n");
-}
- 
-?>
