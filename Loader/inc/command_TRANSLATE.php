0c4c2f50b04fd209b5f95a1496b610ca9967ebb3
Zexorz <mr.metonator@gmail.com>
2015-01-19 00:55:08 +0100

    Fixed russian charset
    to Google: Fuck your badly JSON.

--- Loader/inc/command_TRANSLATE.php
+++ Loader/inc/command_TRANSLATE.php
@@ -21,14 +21,20 @@
 			$reply = @file_get_contents("https://translate.google.com/translate_a/single?client=t&sl=auto&tl=en&hl=pl&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&rom=0&ssel=3&tsel=6&tk=516524|805520&q=".$sendtocb);
 		}
 
-		preg_match("/(.*?)]]/", $reply, $matches);
+		/*preg_match("/(.*?)]]/", $reply, $matches);
 		$antydecode =  json_encode($json);
 		$json = str_replace("[[[", "[", $matches['1'])."]";
 		$json = json_decode($json);
 
 		$reply = $json;
 
-		$reply = trim(preg_replace('/\s\s+/', '', ucfirst($json['0'])));
+		$reply = trim(preg_replace('/\s\s+/', '', ucfirst($json['0'])));*/
+		
+		$reply = substr($reply, 0, 255);
+		$reply = str_replace("[[[", "[", explode("\"]", $reply)['0']."\"]");
+		$reply = json_decode($reply);
+		$reply = $reply['0'];
+		
 		if($reply != NULL) {
 			fputs($socket,"PRIVMSG ".$channelname." :4[{$get_nickname} / Translation Beta]: {$reply}\r\n"); 
 			//fputs($socket,"PRIVMSG ".$channelname." :4[DEBUG]: {$antydecode}\r\n"); 
