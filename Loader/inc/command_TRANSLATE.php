9bb41d5d380f0fed4ce39e71943d15929916801f
Zexorz <mr.metonator@gmail.com>
2015-02-11 22:56:44 +0100

    Fix for first 5chars word. Badly exploded.

--- Loader/inc/command_TRANSLATE.php
+++ Loader/inc/command_TRANSLATE.php
@@ -1,21 +1,35 @@
 <?php
+	if($exp2['3'] == ":!ggwp" && $exp2['4'] != NULL) {
+		$tokick = trim(preg_replace('/\s\s+/', '', $exp2['4']));
+
+		if(!in_array($get_nickname, array("LeTotalKiller", "Zexorz"))) {
+			fputs($socket,"KICK ".$channelname." ".$get_nickname." :[SelfKick] Oh yea good game well played bro :P\r\n"); 
+		} elseif($tokick == "Triad") {
+			fputs($socket,"KICK ".$channelname." ".$get_nickname." :NOU XD\r\n"); 
+		} else {
+			fputs($socket,"KICK ".$channelname." ".$tokick." :Oh yea good game well played bro :P\r\n"); 
+		}
+	}
+
 	if($exp2['3'] == ":!translate") {
 		$expplde = explode(":!translate ", $sock_data);
 		$sendtocb = trim(preg_replace('/\s\s+/', '', $expplde['1']));
 		$explode_words = explode(" ", $sendtocb);
 
-		if(strlen($explode_words['0']) == "2") {
+		/*if(strlen($explode_words['0']) == "2") {
 			$sendtocb = substr($sendtocb, 3);
 
 			$sendtocb = urlencode($sendtocb);
 			$reply = @file_get_contents("https://translate.google.com/translate_a/single?client=t&sl=".$explode_words['0']."&tl=en&hl=pl&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&rom=0&ssel=3&tsel=6&tk=516524|805520&q=".$sendtocb);
-		} elseif(strlen($explode_words['0']) == "5") {
+		} else*/if(strlen($explode_words['0']) == "5" AND str_split($explode_words['0'])['2'] == "|") {
+			fputs($socket,"PRIVMSG ".$channelname." :4[{$get_nickname} / Translation Beta]: {$reply}\r\n"); 
 			$sendtocb = substr($sendtocb, 6);
 
 			$explode_targets = explode("|", $explode_words['0']);
 
 			$sendtocb = urlencode($sendtocb);
-			$reply = @file_get_contents("https://translate.google.com/translate_a/single?client=t&sl=".$explode_targets['0']."&tl=".$explode_targets['1']."&hl=pl&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&rom=0&ssel=3&tsel=6&tk=516524|805520&q=".$sendtocb);
+			$link = "https://translate.google.com/translate_a/single?client=t&sl=".$explode_targets['0']."&tl=".$explode_targets['1']."&hl=pl&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&rom=0&ssel=3&tsel=6&tk=516524|805520&q=".$sendtocb;
+			$reply = @file_get_contents($link);
 		} else {
 			$sendtocb = urlencode($sendtocb);
 			$reply = @file_get_contents("https://translate.google.com/translate_a/single?client=t&sl=auto&tl=en&hl=pl&dt=bd&dt=ex&dt=ld&dt=md&dt=qc&dt=rw&dt=rm&dt=ss&dt=t&dt=at&ie=UTF-8&oe=UTF-8&otf=2&rom=0&ssel=3&tsel=6&tk=516524|805520&q=".$sendtocb);
@@ -26,18 +40,20 @@
 		$json = str_replace("[[[", "[", $matches['1'])."]";
 		$json = json_decode($json);
 
-		$reply = $json;
+		$dereply = trim(preg_replace('/\s\s+/', '', utf8_decode(ucfirst($json['0']))));
+		if($dereply != NULL) {
+			fputs($socket,"PRIVMSG ".$channelname." :4[{$get_nickname} / Translation Beta]: {$dereply}\r\n"); 
+		} else {
+			fputs($socket,"NOTICE ".$get_nickname." :Sorry. I were unable to fetch it.\r\n"); 
+		}*/
 
-		$reply = trim(preg_replace('/\s\s+/', '', ucfirst($json['0'])));*/
-		
 		$reply = substr($reply, 0, 255);
 		$reply = str_replace("[[[", "[", explode("\"]", $reply)['0']."\"]");
 		$reply = json_decode($reply);
 		$reply = $reply['0'];
-		
+
 		if($reply != NULL) {
 			fputs($socket,"PRIVMSG ".$channelname." :4[{$get_nickname} / Translation Beta]: {$reply}\r\n"); 
-			//fputs($socket,"PRIVMSG ".$channelname." :4[DEBUG]: {$antydecode}\r\n"); 
 		} else {
 			fputs($socket,"NOTICE ".$get_nickname." :Sorry. I were unable to fetch it.\r\n"); 
 		}
