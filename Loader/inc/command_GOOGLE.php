45bf634bd43e6dce62e56972e33eb08074028d67
Zexorz <mr.metonator@gmail.com>
2015-02-22 19:11:19 +0100

    Fast update if nothing to search for.

--- Loader/inc/command_GOOGLE.php
+++ Loader/inc/command_GOOGLE.php
@@ -3,35 +3,39 @@
 		$expplde = end(explode(":!google ", $sock_data));
 		$expplde = trim(preg_replace("/\s\s+/", "", $expplde));
 		$google = urlencode($expplde);
-
-		$google = 'http://www.google.com/search?q='.$google.'&btnI=q';
-
-		$google = get_headers($google);
-
-		foreach($google as $loc) {
-			$explode = explode(": ", $loc);
-			if($explode[0] == "Location") {
-				$urls[] = $explode[1];
-			}
-		}
-
-		$count = count($urls)-1;
-
-		$exacturl = $urls[$count];
-		$parseurl = parse_url($exacturl);
-
-		if($parseurl['host'] == "www.google.pl" && $parseurl['path'] == "/search") {
-
-			print_r(get_headers($exacturl));
-
-			$thislink = "Not feeling lucky: ";
-			$thislink .= $exacturl;
+		
+		if($google == NULL) {
+			fputs($socket,"PRIVMSG ".$channelname." :".($get_nickname).": Nothing to search.\r\n");
 		} else {
-			$thislink = $exacturl;
+			$google = 'http://www.google.com/search?q='.$google.'&btnI=q';
+	
+			$google = get_headers($google);
+	
+			foreach($google as $loc) {
+				$explode = explode(": ", $loc);
+				if($explode[0] == "Location") {
+					$urls[] = $explode[1];
+				}
+			}
+	
+			$count = count($urls)-1;
+	
+			$exacturl = $urls[$count];
+			$parseurl = parse_url($exacturl);
+	
+			if($parseurl['host'] == "www.google.pl" && $parseurl['path'] == "/search") {
+	
+				print_r(get_headers($exacturl));
+	
+				$thislink = "Not feeling lucky: ";
+				$thislink .= $exacturl;
+			} else {
+				$thislink = $exacturl;
+			}
+	
+			fputs($socket,"PRIVMSG ".$channelname." :".($thislink)."\r\n");
+	
+			unset($urls);
 		}
-
-		fputs($socket,"PRIVMSG ".$channelname." :".($thislink)."\r\n");
-
-		unset($urls);
 	}
 ?>
