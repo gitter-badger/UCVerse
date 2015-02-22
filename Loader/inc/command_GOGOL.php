b4f14f841fff1be66912afe5b8a6186ce183135f
Zexorz <mr.metonator@gmail.com>
2015-02-22 19:09:29 +0100

    Fast update if nothing to search for.

--- Loader/inc/command_GOGOL.php
+++ Loader/inc/command_GOGOL.php
@@ -3,35 +3,39 @@
 		$expplde = end(explode(":!gogol ", $sock_data));
 		$gogol = trim(preg_replace("/\s\s+/", "", $expplde));
 
-		$url = "http://gogol.librelogiciel.com/cgi-bin/gogol.cgi";
+		if($gogol == NULL) {
+			fputs($socket,"PRIVMSG ".$channelname." :You're so unlucky ".$get_nickname." that i dont even know what you want.\r\n");
+		} else {
+			$url = "http://gogol.librelogiciel.com/cgi-bin/gogol.cgi";
 
-		$postdata = http_build_query(
-			array(
-				'q' => $gogol,
-				'_gogol' => '_gogol',
-				'_charset_' => NULL,
-				'btnI' => "I'm feeling unlucky with Gogol !"
-			)
-		);
+			$postdata = http_build_query(
+				array(
+					'q' => $gogol,
+					'_gogol' => '_gogol',
+					'_charset_' => NULL,
+					'btnI' => "I'm feeling unlucky with Gogol !"
+				)
+			);
 
-		$opts = array('http' =>
-			array(
-				'method'  => 'POST',
-				'header'  => 'Content-type: application/x-www-form-urlencoded',
-				'content' => $postdata
-			)
-		);
+			$opts = array('http' =>
+				array(
+					'method'  => 'POST',
+					'header'  => 'Content-type: application/x-www-form-urlencoded',
+					'content' => $postdata
+				)
+			);
 
-		$context  = stream_context_create($opts);
-		$stream = fopen($url, 'r', false, $context);
+			$context  = stream_context_create($opts);
+			$stream = fopen($url, 'r', false, $context);
 
-		$meta = stream_get_meta_data($stream);
-		$headers = $meta['wrapper_data'];
-		$location = $headers['3'];
+			$meta = stream_get_meta_data($stream);
+			$headers = $meta['wrapper_data'];
+			$location = $headers['3'];
 
-		$location = str_replace("Location: ", "", $location);
+			$location = str_replace("Location: ", "", $location);
 
-		fputs($socket,"PRIVMSG ".$channelname." :".($location)."\r\n");
-		unset($meta);
+			fputs($socket,"PRIVMSG ".$channelname." :".($location)."\r\n");
+			unset($meta);
+		}
 	}
 ?>
